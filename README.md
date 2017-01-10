3078f2c72a5afdbe786a309d2c5415cce4a73f2d
Steve Oliver <steve@circatree.com>
2017-01-10 11:57:12 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Commerce Crude Shipping
+
+A module that provides support for crude (flat-rate) shipping charges for purchasable entities that implement a 'field_flat_rate_shipping_amount' field.
+
+## Usage
+
+- Enable this module.
+- Create a 'field_flat_rate_shipping_amount' Commerce Price field on product variation types or other purchasable entities.
+- Populate the field(s) with amounts.
---
+++ src/OrderProcessor/FlatRate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\commerce_crude_shipping\OrderProcessor;
+
+use Drupal\commerce_order\Adjustment;
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_order\OrderProcessorInterface;
+use Drupal\commerce_price\Price;
+
+class FlatRate implements OrderProcessorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process(OrderInterface $order) {
+    $adjustment = new Price('0', $order->getTotalPrice()->getCurrencyCode());
+    foreach ($order->getItems() as $item) {
+      /** @var \Drupal\commerce_order\Entity\OrderItem $item */
+      $entity = $item->getPurchasedEntity();
+      $charge = $entity->get('field_flat_rate_shipping_amount')->getValue();
+      if (!empty($charge[0])) {
+        $rate = new Price($charge[0]['number'], $charge[0]['currency_code']);
+        $adjustment = $adjustment->add($rate->multiply($item->getQuantity()));
+      }
+    }
+    if (!$adjustment->isZero()) {
+      $order->addAdjustment(new Adjustment([
+        'type' => 'shipping',
+        'label' => 'Shipping',
+        'amount' => $adjustment,
+      ]));
+    }
+  }
+
+}
