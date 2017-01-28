fb58501e473e28db40bdd5f7f54cc25cc9774947
Steve Oliver <steve@circatree.com>
2017-01-28 10:05:37 -0800

    Fix charge.

--- src/OrderProcessor/FlatRate.php
+++ src/OrderProcessor/FlatRate.php
@@ -18,10 +18,10 @@ class FlatRate implements OrderProcessorInterface {
       /** @var \Drupal\commerce_order\Entity\OrderItem $item */
       $entity = $item->getPurchasedEntity();
       if ($entity->field_flat_rate_shipping_amount) {
-        $charge = $entity->field_flat_rate_shipping_amount->value;
-        if (!empty($charge[0])) {
-          $rate = new Price($charge[0]['number'], $charge[0]['currency_code']);
-          $adjustment = $adjustment->add($rate->multiply($item->getQuantity()));
+        /** @var Drupal\commerce_price\Plugin\Field\FieldType\PriceItem $charge */
+        $charge = $entity->field_flat_rate_shipping_amount->first();
+        if (!empty($charge)) {
+          $adjustment = $adjustment->add($charge->toPrice()->multiply($item->getQuantity()));
         }
       }
     }
